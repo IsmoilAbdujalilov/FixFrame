1cffb6047a21cc4b29f7b1c75f63181dc3fa5d96
IsmoilAbdujalilov <abdujalilovismoilgithub@gmail.com>
2024-07-06 11:35:15 +0500

    feat: Added services responsive sentences codes

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -104,6 +104,145 @@ const Services = () => {
             })}
         </ul>
       </div>
+
+      <div className="container">
+        <ul className="services__list">
+          <li className="services__item">
+            <h3 className="services__item-title">Our Expertise:</h3>
+            <ul className="services__sentences">
+              <li className="services__sentence">
+                <p className="services__sentence-text">
+                  <strong>Structural Repairs</strong>: We address and repair any
+                  structural damage to your property, ensuring it is safe and
+                  stable. Our team is experienced in handling damages caused by
+                  natural disasters, accidents, or wear and tear.
+                </p>
+              </li>
+              <li className="services__sentence">
+                <p className="services__sentence-text">
+                  <strong>Water Damage Restoration</strong>: Our water damage
+                  restoration services include water extraction, drying,
+                  dehumidification, and mold remediation. We work quickly to
+                  minimize damage and restore your property to its original
+                  state
+                </p>
+              </li>
+              <li className="services__sentence">
+                <p className="services__sentence-text">
+                  <strong>Fire and Smoke Damage Restoration:</strong> We provide
+                  comprehensive fire and smoke damage restoration services,
+                  including soot and smoke removal, odor control, and property
+                  repair. Our goal is to restore your property and eliminate any
+                  traces of fire damage.
+                </p>
+              </li>
+              <li className="services__sentence">
+                <p className="services__sentence-text">
+                  <strong>Electrical and Plumbing Repairs:</strong> Our skilled
+                  technicians handle all types of electrical and plumbing
+                  repairs, from fixing faulty wiring and leaky pipes to
+                  replacing damaged fixtures. We ensure that all repairs are
+                  performed safely and meet industry standards.
+                </p>
+              </li>
+              <li className="services__sentence">
+                <p className="services__sentence-text">
+                  <strong>Interior and Exterior Repairs:</strong> We offer a
+                  wide range of interior and exterior repair services, including
+                  drywall repair, painting, roofing, siding, and window
+                  replacement. Our team ensures that all repairs blend
+                  seamlessly with the existing structure and enhance the overall
+                  appearance of your property.
+                </p>
+              </li>
+            </ul>
+          </li>
+          <li className="services__item">
+            <h3 className="services__item-title">Why Choose Us?</h3>
+            <ul className="services__sentences">
+              <li className="services__sentence">
+                <p className="services__sentence-text">
+                  <strong>Structural Repairs:</strong> We address and repair any
+                  structural damage to your property, ensuring it is safe and
+                  stable. Our team is experienced in handling damages caused by
+                  natural disasters, accidents, or wear and tear.
+                </p>
+              </li>
+              <li className="services__sentence">
+                <p className="services__sentence-text">
+                  <strong>Water Damage Restoration</strong>: Our water damage
+                  restoration services include water extraction, drying,
+                  dehumidification, and mold remediation. We work quickly to
+                  minimize damage and restore your property to its original
+                  state
+                </p>
+              </li>
+              <li className="services__sentence">
+                <p className="services__sentence-text">
+                  <strong>Fire and Smoke Damage Restoration:</strong> We provide
+                  comprehensive fire and smoke damage restoration services,
+                  including soot and smoke removal, odor control, and property
+                  repair. Our goal is to restore your property and eliminate any
+                  traces of fire damage.
+                </p>
+              </li>
+              <li className="services__sentence">
+                <p className="services__sentence-text">
+                  <strong>Electrical and Plumbing Repairs:</strong> Our skilled
+                  technicians handle all types of electrical and plumbing
+                  repairs, from fixing faulty wiring and leaky pipes to
+                  replacing damaged fixtures. We ensure that all repairs are
+                  performed safely and meet industry standards.
+                </p>
+              </li>
+              <li className="services__sentence">
+                <p className="services__sentence-text">
+                  <strong>Interior and Exterior Repairs:</strong> We offer a
+                  wide range of interior and exterior repair services, including
+                  drywall repair, painting, roofing, siding, and window
+                  replacement. Our team ensures that all repairs blend
+                  seamlessly with the existing structure and enhance the overall
+                  appearance of your property.
+                </p>
+              </li>
+            </ul>
+          </li>
+          <li className="services__item">
+            <h3 className="services__item-title">Our Services:</h3>
+            <ul className="services__sentences">
+              <li className="services__sentence">
+                <p className="services__sentence-text">
+                  <strong>Property Inspections:</strong> Comprehensive
+                  evaluations conducted by experienced inspectors, focusing on
+                  safety, maintenance needs, and compliance with regulations. We
+                  provide detailed reports and recommendations to help you make
+                  informed decisions.
+                </p>
+              </li>
+              <li className="services__sentence">
+                <p className="services__sentence-text">
+                  <strong>Lawn Maintenance:</strong> Regular care plans
+                  customized to your lawn's specific needs and seasonal
+                  requirements. Services include regular mowing, trimming,
+                  edging, fertilization, weed management, and treatments to
+                  promote healthy growth and lush greenery.
+                </p>
+              </li>
+              <li className="services__sentence">
+                <p className="services__sentence-text">
+                  <strong>Quality and Satisfaction:</strong> At FixFrame, we are
+                  committed to delivering exceptional service and superior
+                  results in inspection services and lawn maintenance. Whether
+                  you're looking to enhance curb appeal, maintain property
+                  value, or ensure a safe and inviting outdoor environment, you
+                  can trust us to provide reliable, professional, and
+                  personalized care.
+                </p>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
